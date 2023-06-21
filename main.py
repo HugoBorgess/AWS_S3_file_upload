fc3bc515163d32fd4e0ecd00f86601d557ec5430
HugoBorgess <104326413+HugoBorgess@users.noreply.github.com>
2023-06-21 13:09:14 -0300

    Criando o arquivo main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+ACCESS_KEY = 'XXXXXXXXXXXXXXXXX'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+LOCAL_FILE = 'local_file_name'
+BUCKET_NAME = 'bucket_name'
+S3_FILE_NAME = 'file_name_on_s3'
+
+def upload_to_s3(local_file, bucket, s3_file):
+    ## Essa função é responsável por carregar o arquivo no bucket do S3 usando as credenciais especificadas.
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+result = upload_to_s3(LOCAL_FILE, BUCKET_NAME, S3_FILE_NAME)
